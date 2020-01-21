a4700a9e5829a9b41ec67811e12867d90f2c1550
konradDlugosz <1812755@brunel.ac.uk>
2020-01-21 12:33:38 +0000

    Added map

--- app/src/main/java/android/example/travelsuggestion/LetMeChoose.java
+++ app/src/main/java/android/example/travelsuggestion/LetMeChoose.java
@@ -3,12 +3,24 @@ package android.example.travelsuggestion;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class LetMeChoose extends AppCompatActivity {
 
+    private Button btn_example;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_let_me_choose);
+
+        btn_example = (Button) findViewById(R.id.btnExample);
+    }
+
+    public void onClickShowExampleCoutry(View view){
+        Toast.makeText(getApplicationContext(), "Its working",Toast.LENGTH_LONG).show();
+
     }
 }
